313f9fd2beec3b78f6d9051967d94b36aba97946
tuan anh nguyen <nguyentuananh23031997@gmail.com>
2018-12-09 01:01:04 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const Joi = require('joi');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 
