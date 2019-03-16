fcf9d0ad9de9e52c5cde5e461e7e0f7b5dede90c
tuan anh <nguyentuananh230319972gmail.com>
2019-03-17 00:10:44 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,29 +1,39 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+// import express from 'express';
+// import bodyParser from 'body-parser';
 
-// router
-import user from './route/user';
-import flower from './route/flower';
-import upload from './route/upload';
-import image from './route/image';
+// // router
+// import user from './route/user';
+// import flower from './route/flower';
+// import upload from './route/upload';
+// import image from './route/image';
 
-const app = express();
-const port = process.env.PORT;
-app.set('port', port || 5000);
+// const app = express();
+// const port = process.env.PORT;
+// app.set('port', port || 5000);
 
-app.use(bodyParser.json({ limit: '200mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '200mb', extended: true }));
-app.use(express.json());
+// app.use(bodyParser.json({ limit: '200mb', extended: true }));
+// app.use(bodyParser.urlencoded({ limit: '200mb', extended: true }));
+// app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+// app.get('/', (req, res) => {
+//   res.send('hello world');
+// });
 
-app.use('/api/upload', upload);
-app.use('/api/image', image);
-app.use('/api/flower', flower);
-app.use('/api/user', user);
+// app.use('/api/upload', upload);
+// app.use('/api/image', image);
+// app.use('/api/flower', flower);
+// app.use('/api/user', user);
 
-app.listen(app.get('port'), () => {
-  console.log('Node server is running on port ' + app.get('port'));
-});
+// app.listen(app.get('port'), () => {
+//   console.log('Node server is running on port ' + app.get('port'));
+// });
+
+var http = require('http');
+
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+  })
+  .listen(5000, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:3000/');
