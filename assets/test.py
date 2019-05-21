c016c09249bd37fa7972b102e699dfa0552ce8d6
tuan anh <nguyentuananh230319972gmail.com>
2019-05-21 11:17:29 +0700

    update

--- assets/test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-with open("text.txt", "w+") as file:
-    name = os.listdir('./userIcon')
-    print name
-    for name_line in name:
-        name_line_arr = name_line.split('-')
-        file.write(
-            "{'index':"+str(int(name_line_arr[0])) + ",'name':'" + name_line_arr[1]+"','image':'"+name_line + "'},\n")
